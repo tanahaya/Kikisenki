01e7b81619dc09ff1ba017af6dc68002e00b974f
tanahaya <tanahaya00623@gmail.com>
2019-09-14 20:46:13 +0900

    次の会話に行く機能を追加。また、最後の会話の後にもう一度押すとselectSceneに行く機能も追加。

--- ConversationScene.swift
+++ ConversationScene.swift
@@ -16,12 +16,12 @@ class ConversationScene : SKScene, SKPhysicsContactDelegate{
     let nameLabel = SKLabelNode()
     let serifLabel = SKLabelNode()//文字を表示する。
     
-    let nameArray:[String] = ["フランソワ","2","3","ゴリラゴリあ"]
+    var serifArray:[String] = []
     
-    let serifArray:[String] = ["フランソワ","2","3","ゴリラゴリあ"]
+    var pageNumber:Int = 0
     
-    
-    
+    var maxPageNumber = 0
+ 
     override func didMove(to view: SKView) {
         
         var list:[String] = []//一度入れる。
@@ -48,12 +48,13 @@ class ConversationScene : SKScene, SKPhysicsContactDelegate{
                 
                 let detail = data.components(separatedBy: ",")
                 print("【id】\(detail[0])　【名前】\(detail[1])　【説明】\(detail[2])")
+                serifArray.append(data)
                 
             }
             
         }
         
-        
+        maxPageNumber = serifArray.count//最大ページ数を追加。
         
         //起動した時の処理
         self.size = CGSize(width: 896, height: 414)//896x414が最適。これはiphoneXRの画面サイズを横にしたもの。
@@ -72,26 +73,55 @@ class ConversationScene : SKScene, SKPhysicsContactDelegate{
         nameLabel.fontColor = UIColor.black// 色を指定(青)
         nameLabel.horizontalAlignmentMode = .left
         nameLabel.position = CGPoint(x: 100, y: 170) // 表示するポジションを指定.今回は中央
-        nameLabel.text = nameArray[0]
         //print(nameArray[0].utf16.count)=>文字数を数える。
         self.addChild(nameLabel)//シーンに追加
         
         
         serifLabel.fontSize = 27 // フォントサイズを設定.
         serifLabel.fontColor = UIColor.black// 色を指定(青).
-        serifLabel.text = "セリフあああああああああ"
         serifLabel.horizontalAlignmentMode = .left
-        
         if serifLabel.numberOfLines == 1 {
             serifLabel.position = CGPoint(x: 80, y: 120)// 表示するポジションを指定.今回は中央
         } else if serifLabel.numberOfLines == 2 {
             serifLabel.position = CGPoint(x: 80, y: 70)// 表示するポジションを指定.今回は中央
+        } else if serifLabel.numberOfLines == 3 {
+            serifLabel.position = CGPoint(x: 80, y: 70)// 表示するポジションを指定.今回は中央
         }
         self.addChild(serifLabel)//シーンに追加
+        
+        self.text(page: pageNumber)
         
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        pageNumber = pageNumber + 1
+        
+        if pageNumber == maxPageNumber {//これ以上表示するものがない時。
+            
+            self.gotoSelectScene()
+            
+        } else {
+            self.text(page: pageNumber)
+        }
+        
+    }
+    
+    func text(page:Int) {
+        
+        let detail = serifArray[page].components(separatedBy: ",")
+        nameLabel.text = "\(detail[1])"
+        serifLabel.text = "\(detail[2])"
+        
+    }
+    
+    func gotoSelectScene() {
+        
+        let Scene = SelectScene()
+        Scene.size = self.size
+        let transition = SKTransition.crossFade(withDuration: 1.0)
+        
+        self.view?.presentScene(Scene, transition: transition)
         
     }
     
